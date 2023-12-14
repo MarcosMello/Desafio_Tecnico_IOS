1c2b9932fc72c5072b46ff6c2f523711ec44fb45
MarcosMello <marcosvinicius-vm@outlook.com>
2023-12-13 22:20:23 -0300

    Feat: Adds secondaryDict, and functions related, to handle Artists' search

--- Jukebox.playground/Sources/CustomDictionary.swift
+++ Jukebox.playground/Sources/CustomDictionary.swift
@@ -2,21 +2,34 @@ import Foundation
 
 public class CustomDictionary<T> {
 
-    private var dict = [String: Array<T>]()
+    private var mainDict = [String: Array<T>]()
+    private var secondaryDict = [String: Array<T>]()
 
     public init(){}
-    
-    subscript(key: String) -> Array<T>? {
-        return dict["string: \(key)"]
-    }
-
-    subscript(key: UInt) -> Array<T>? {
-        return dict["int: \(key)"]
-    }
 }
 
-protocol CountFunctions{
+protocol BasicFunctions{
+    associatedtype T
+    
+    subscript(key: String) -> Array<T>? { get }
+
+    subscript(key: UInt) -> Array<T>? { get }
+    
     var count: Int { get }
+}
+
+extension CustomDictionary: BasicFunctions{
+    public subscript(key: String) -> Array<T>? {
+        return mainDict["string: \(key)"]
+    }
+
+    public subscript(key: UInt) -> Array<T>? {
+        return mainDict["int: \(key)"]
+    }
+    
+    public var count: Int{
+        mainDict.count
+    }
 }
 
 protocol MainFunctions{
@@ -33,55 +46,117 @@ protocol MainFunctions{
 extension CustomDictionary: MainFunctions where T == Music{
     @discardableResult
     public func add(_ instantiatedT: T) -> Bool{
-        if dict["int: \(instantiatedT.id)"] != nil{
+        if mainDict["int: \(instantiatedT.id)"] != nil{
             return false
         }
         
-        dict["int: \(instantiatedT.id)"] = [instantiatedT]
+        mainDict["int: \(instantiatedT.id)"] = [instantiatedT]
         
-        guard let musics = dict["string: \(instantiatedT.name)"] else{
-            dict["string: \(instantiatedT.name)"] = [instantiatedT]
-            
-            return true
-        }
+        mainDict["string: \(instantiatedT.name)"] = 
+            mainDict["string: \(instantiatedT.name)"] == nil ?
+                [instantiatedT] :
+                mainDict["string: \(instantiatedT.name)"]! + [instantiatedT]
         
-        dict["string: \(instantiatedT.name)"] = musics + [instantiatedT]
+        
+        secondaryDict["int: \(instantiatedT.artist.id)"] =
+            secondaryDict["int: \(instantiatedT.artist.id)"] == nil ?
+                [instantiatedT] :
+                secondaryDict["int: \(instantiatedT.artist.id)"]! + [instantiatedT]
+        
+        secondaryDict["string: \(instantiatedT.artist.name)"] =
+            secondaryDict["string: \(instantiatedT.artist.name)"] == nil ?
+                [instantiatedT] :
+                secondaryDict["string: \(instantiatedT.artist.name)"]! + [instantiatedT]
         
         return true
     }
 
     public func removeAll(forKey key: String){
-        guard let musics = dict["string: \(key)"] else{
+        guard let instancesOfT = mainDict["string: \(key)"] else{
             return
         }
         
-        dict.removeValue(forKey: "string: \(key)")
+        mainDict.removeValue(forKey: "string: \(key)")
         
-        for music in musics{
-            dict.removeValue(forKey: "int: \(music.id)")
+        for instanceOfT in instancesOfT{
+            mainDict.removeValue(forKey: "int: \(instanceOfT.id)")
+            removeMusicFromArtist(instantiatedT: instanceOfT)
         }
     }
 
     public func removeAll(forKey key: UInt){ 
-        guard let musics = dict["int: \(key)"] else{
+        guard let instancesOfT = mainDict["int: \(key)"] else{
             return
         }
         
-        dict.removeValue(forKey: "int: \(key)")
+        mainDict.removeValue(forKey: "int: \(key)")
         
-        for music in musics{
-            let musicsByName: Array<Music>? = dict["string: \(music.name)"]
+        for instanceOfT in instancesOfT{
+            removeMusicFromArtist(instantiatedT: instanceOfT)
             
-            guard let musicsByName = musicsByName else{
+            guard let instancesOfTByName = mainDict["string: \(instanceOfT.name)"] else{
                 continue
             }
             
-            if musicsByName.count > 1{
-                dict["string: \(music.name)"] = musicsByName.filter { $0 != music }
+            if instancesOfTByName.count > 1{
+                mainDict["string: \(instanceOfT.name)"] = instancesOfTByName.filter { $0 != instanceOfT }
                 continue
             }
             
-            dict.removeValue(forKey: "string: \(music.name)")
+            mainDict.removeValue(forKey: "string: \(instanceOfT.name)")
         }
+    }
+    
+    public func removeMusicFromArtist(instantiatedT: T){
+        let instancesOfTByArtistId: Array<T>? = secondaryDict["int: \(instantiatedT.artist.id)"]
+        let instancesOfTByArtistName: Array<T>? = secondaryDict["string: \(instantiatedT.artist.name)"]
+        
+        if instancesOfTByArtistId != nil && instancesOfTByArtistId!.count > 1{
+            secondaryDict["int: \(instantiatedT.artist.id)"] = instancesOfTByArtistId!.filter { $0 != instantiatedT }
+        } else {
+            secondaryDict.removeValue(forKey: "int: \(instantiatedT.artist.id)")
+        }
+        
+        if instancesOfTByArtistName != nil && instancesOfTByArtistName!.count > 1{
+            secondaryDict["string: \(instantiatedT.artist.name)"] = instancesOfTByArtistName!.filter { $0 != instantiatedT }
+        } else {
+            secondaryDict.removeValue(forKey: "string: \(instantiatedT.artist.name)")
+        }
+    }
+    
+    public func getMusic(forKey key: String) -> Array<T>?{
+        mainDict["string: \(key)"]
+    }
+    
+    public func getMusic(forKey key: UInt) -> T?{
+        mainDict["int: \(key)"]?.first
+    }
+    
+    public func getMusicsFromArtist(forKey key: String) -> Array<T>?{
+        secondaryDict["string: \(key)"]
+    }
+    
+    public func getMusicsFromArtist(forKey key: UInt) -> Array<T>?{
+        secondaryDict["int: \(key)"]
+    }
+    
+    public func getAllMusics() -> Array<T>?{
+        let idKeys: Array<String>? = mainDict.keys.filter { $0.starts(with: "int: ") }
+        
+        guard let idKeys = idKeys else{
+            return nil
+        }
+        
+        var response: Array<T> = Array()
+        
+        for idKey in idKeys{
+            guard let music = mainDict[idKey] else{
+                continue
+            }
+            
+            response += music
+        }
+        
+        return response
     }
 }
